d6a9f27434a86d4be4ce678f73594548ffa3cdbd
ebuleje-dev <edgard.bulejev@hotmail.com>
2025-12-18 15:56:34 -0500

    step1 - maven plugins

--- src/main/java/com/bankx/transactions/application/service/TransactionService.java
+++ src/main/java/com/bankx/transactions/application/service/TransactionService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
-import reactor.core.scheduler.Schedulers;
 
 import java.math.BigDecimal;
 import java.time.Instant;
